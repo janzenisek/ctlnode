65d08df42693df9f40b314d619c11af73ef04095
Jan Zenisek <janzenisek@gmx.at>
2013-11-05 02:23:06 +0100

    updated redirection process

--- app.js
+++ app.js
@@ -15,37 +15,68 @@ app.configure(function () {
 });
 
 app.get('/', function (req, res) {
-    // create new task list (new random url) and link to it and welcome the new user
-
-    res.render('index', {
-        list: {
-            alias: 'GEN',
-            creationMoment: ' [new list on generated url here]',
-            tasks: []
-        }
+    // create new random url and link to it
+    console.log("GOT URL WITHOUT ALIAS");
+    makeNewAlias(function (newAlias) {
+        res.redirect(307, '/' + newAlias);
     });
+    
 });
 
 app.get('/:alias', function (req, res) {
+    if (req.params.alias === 'favicon.ico') return;
+
+    console.log("GOT URL WITH ALIAS: ", req.params.alias);
+    var renderList = function (reqList) {
+        console.log("RENDER LIST: "+ reqList.alias);
+        res.render('index', {
+            list: reqList
+        });
+    }
+
     // find mapped list (if existent) in mongodb and render the list
     // if there is no list for alias create new task list on the alias and link to it and welcome the new user
 
     lists.findOne(req.params.alias, function (err, result) {
         if (err) {
+            // !!! update error handling !!!
             console.log(err);
-        } else {
-            var reqList = result || {
-                alias: 'CHOSEN',
-                creationMoment: ' [new list on chosen url here]',
-                tasks: []
-            };
-
-            res.render('index', {
-                list: reqList
-            });
+        } else {          
+            if (result) {
+                renderList(result);
+            } else {
+                lists.create(req.params.alias, function (err, result) {
+                    if (err) {
+                        // !!! update error handling !!!
+                        console.log(err);
+                    } else {
+                        console.log("CREATE NEW LIST: ", result[0].alias);
+                        renderList(result[0]);
+                    }
+                });
+            }
         }
     });
 });
 
 // bind app to server-specified listening-port or development-default port 3000
 var server = http.createServer(app).listen(process.env.PORT || 3000);
+
+function makeNewAlias(callback) {
+    var alias = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 10; i++)
+        alias += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    // proove against DB, and call function recursivly in case of already existing alias
+    lists.findOne(alias, function (err, result) {
+        if (!result) {
+            console.log("MADE A NEW ALIAS: " + alias);
+            return callback(alias);
+        } else {
+            console.log("ALIAS ALREADY EXSISTS - ANOTHER GENERATION TRY STARTS");
+            makeNewAlias(callback);
+        }
+    });   
+}
